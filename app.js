2155f968863c9d42095d8153550d5dd4324a394c
Abhay Ruparel <abhayruparel2000@gmail.com>
2021-10-18 11:49:03 +0530

    nodeRoute: add routing to php files. rename html->php
    Signed-off-by: Abhay Ruparel <abhayruparel2000@gmail.com>

--- app.js
+++ app.js
@@ -13,26 +13,28 @@ router.use(function(req,res,next){
 router.get('/', function(req,res){
 	res.redirect("http://localhost:8080/");
 });
+router.get('/image', function(req,res){
+	// res.redirect("http://localhost:8080/");
+	res.redirect("http://localhost:8080/image.php");
+});
+router.get('/persistence', function(req,res){
+	// res.redirect("http://localhost:8080/");
+	res.redirect("http://localhost:8080/persistence.php");
+});
+router.get('/recommend', function(req,res){
+	// res.redirect("http://localhost:8080/");
+	res.redirect("http://localhost:8080/recommend.php");
+});
+router.get('/login', function(req,res){
+	// res.redirect("http://localhost:8080/");
+	res.redirect("http://localhost:8080/signin.php");
+	console.log("redirected to login page");
+});
 
-// router.get('/image', function(req,res){
-// 	res.sendFile(path + 'image.html');
-// });
-// router.get('/persistence', function(req,res){
-// 	res.sendFile(path + 'persistence.html');
-// });
-// router.get('/recommend', function(req,res){
-// 	res.sendFile(path + 'recommend.html');
-// });
-router.get('/signin', function(req,res){
-	// res.writeHead(301, {Location: 'http://localhost/tea_store/views/signin.php'});
-	res.redirect('http://localhost/tea_store/views/signin.php');
-});
-router.get('/signin.php',function(req,res){
-res.send('sign')
-});
+
 app.use(express.static(path));
 app.use('/',router);
 
 app.listen(port, function () {
-	console.log('example app listening on port 8001!!')
+	console.log('routing app listening on port 8001!')
 })
