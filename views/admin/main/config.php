104d5256c1bcf96df7fe016b7558821823469974
Abhay Ruparel <abhayruparel2000@gmail.com>
2021-11-09 10:27:07 +0530

    admin: use environment varibles
    Signed-off-by: Abhay Ruparel <abhayruparel2000@gmail.com>

--- views/admin/main/config.php
+++ views/admin/main/config.php
@@ -1,10 +1,12 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'mysql');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'secret');
-define('DB_NAME', 'tea_store');
+
+define('DB_SERVER_TEA_STORE', getenv(DB_SERVER_TEA_STORE));
+define('DB_USERNAME_TEA_STORE', getenv(DB_USERNAME_TEA_STORE));
+define('DB_PASSWORD_TEA_STORE', getenv(DB_PASSWORD_TEA_STORE));
+define('DB_NAME_TEA_STORE', getenv(DB_NAME_TEA_STORE));
+
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
