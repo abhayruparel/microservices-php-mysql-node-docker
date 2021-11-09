4d78ed6a7f7976c78ba01975d93afa8ce1c236bc
Abhay Ruparel <abhayruparel2000@gmail.com>
2021-11-09 12:08:58 +0530

    config.php: add quotes T_T

--- views/admin/main/config.php
+++ views/admin/main/config.php
@@ -2,10 +2,10 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 
-define('DB_SERVER_TEA_STORE', getenv(DB_SERVER_TEA_STORE));
-define('DB_USERNAME_TEA_STORE', getenv(DB_USERNAME_TEA_STORE));
-define('DB_PASSWORD_TEA_STORE', getenv(DB_PASSWORD_TEA_STORE));
-define('DB_NAME_TEA_STORE', getenv(DB_NAME_TEA_STORE));
+define('DB_SERVER_TEA_STORE', getenv('DB_SERVER_TEA_STORE'));
+define('DB_USERNAME_TEA_STORE', getenv('DB_USERNAME_TEA_STORE'));
+define('DB_PASSWORD_TEA_STORE', getenv('DB_PASSWORD_TEA_STORE'));
+define('DB_NAME_TEA_STORE', getenv('DB_NAME_TEA_STORE'));
 
  
 /* Attempt to connect to MySQL database */
